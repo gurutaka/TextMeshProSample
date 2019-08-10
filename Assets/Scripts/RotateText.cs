322b5591abf3b1e36aa421291fb9e0821fc9b8e9
Takashi Irita <irt.tks0311@gmail.com>
2019-08-10 14:49:08 +0900

    fix rotate script

--- Assets/Scripts/RotateText.cs
+++ Assets/Scripts/RotateText.cs
@@ -1,41 +1,33 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
-public class RotateText : MonoBehaviour
-{
+public class RotateText : MonoBehaviour {
     [SerializeField]
     private float animationSpeed = 1;
     [SerializeField]
-    [Range(0, 360)] int maxRotation = 180;
+    [Range (0, 360)] int maxRotation = 180;
 
     private TextMeshPro textMeshPro;
-    private void Awake()
-    {
-        textMeshPro = gameObject.GetComponent<TextMeshPro>();
+    private void Awake () {
+        textMeshPro = gameObject.GetComponent<TextMeshPro> ();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update () {
         // メッシュ更新
-        textMeshPro.ForceMeshUpdate();
+        textMeshPro.ForceMeshUpdate ();
 
         var textInfo = textMeshPro.textInfo;
-        if (textInfo.characterCount == 0)
-        {
+        if (textInfo.characterCount == 0) {
             return;
         }
 
-
-
         //文字毎にloop
-        for (int index = 0; index < textInfo.characterCount; index++)
-        {
+        for (int index = 0; index < textInfo.characterCount; index++) {
             var charaInfo = textInfo.characterInfo[index];
 
             //ジオメトリない文字はスキップ
-            if (!charaInfo.isVisible)
-            {
+            if (!charaInfo.isVisible) {
                 continue;
             }
 
@@ -48,10 +40,7 @@ public class RotateText : MonoBehaviour
             //頂点(dest->destinationの略)
             Vector3[] destVertices = textInfo.meshInfo[materialIndex].vertices;
 
-            //移動値
-            float sinValue = Mathf.Sin(Time.time * animationSpeed + 10 * index);
-
-            float angle = maxRotation * Mathf.Sin(Time.time + 10 * index);
+            float angle = maxRotation * Mathf.Sin (Time.time + 10 * index);
 
             Vector3 rotatedCenterVertex = (destVertices[vertexIndex + 1] + destVertices[vertexIndex + 2]) / 2;
 
@@ -61,14 +50,13 @@ public class RotateText : MonoBehaviour
             destVertices[vertexIndex + 2] += -offset;
             destVertices[vertexIndex + 3] += -offset;
 
-
             //回転
-            Matrix4x4 matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0, 0, angle), Vector3.one);
+            Matrix4x4 matrix = Matrix4x4.TRS (Vector3.zero, Quaternion.Euler (0, 0, angle), Vector3.one);
 
-            destVertices[vertexIndex + 0] = matrix.MultiplyPoint3x4(destVertices[vertexIndex + 0]);
-            destVertices[vertexIndex + 1] = matrix.MultiplyPoint3x4(destVertices[vertexIndex + 1]);
-            destVertices[vertexIndex + 2] = matrix.MultiplyPoint3x4(destVertices[vertexIndex + 2]);
-            destVertices[vertexIndex + 3] = matrix.MultiplyPoint3x4(destVertices[vertexIndex + 3]);
+            destVertices[vertexIndex + 0] = matrix.MultiplyPoint3x4 (destVertices[vertexIndex + 0]);
+            destVertices[vertexIndex + 1] = matrix.MultiplyPoint3x4 (destVertices[vertexIndex + 1]);
+            destVertices[vertexIndex + 2] = matrix.MultiplyPoint3x4 (destVertices[vertexIndex + 2]);
+            destVertices[vertexIndex + 3] = matrix.MultiplyPoint3x4 (destVertices[vertexIndex + 3]);
 
             destVertices[vertexIndex + 0] += offset;
             destVertices[vertexIndex + 1] += offset;
@@ -77,11 +65,10 @@ public class RotateText : MonoBehaviour
         }
 
         //ジオメトリ更新
-        for (int i = 0; i < textInfo.meshInfo.Length; i++)
-        {
+        for (int i = 0; i < textInfo.meshInfo.Length; i++) {
             //メッシュ情報
             textInfo.meshInfo[i].mesh.vertices = textInfo.meshInfo[i].vertices;
-            textMeshPro.UpdateGeometry(textInfo.meshInfo[i].mesh, i);
+            textMeshPro.UpdateGeometry (textInfo.meshInfo[i].mesh, i);
         }
     }
 }
